a6570d4a6cf4ba1ae427f5cd55e06532b882b064
wm <531431988@qq.com>
2020-06-01 11:51:54 +0800

    清理文件

---
+++ src/pages/tabBar/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <view>
+首页
+  </view>
+</template>
+
+<script>
+export default {
+  components: {
+    
+  },
+  data () {
+    return {
+
+    }
+  },
+  onLoad () {
+
+  },
+  onShow()  {
+
+  },
+  methods: {
+
+  },
+  onUnload() {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/pages/tabBar/mine.vue
@@ -0,0 +1,34 @@
+<template>
+  <view>
+我的
+  </view>
+</template>
+
+<script>
+export default {
+  components: {
+    
+  },
+  data () {
+    return {
+
+    }
+  },
+  onLoad () {
+
+  },
+  onShow()  {
+
+  },
+  methods: {
+
+  },
+  onUnload() {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
